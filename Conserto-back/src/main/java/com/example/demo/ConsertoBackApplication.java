feb059d1605330f8a32b197c2aedbb70f2fa2dda
Kévin Calmant <kevin.calmant@outlook.com>
2019-07-12 00:20:24 +0200

    Le bug se trouvait au niveau de spring security.
    J'ai fais un httpBasic().disable() et depuis il n'y a plus la page login
    et le reste des API ont l'air fonctionnelles (mis à part un bug au niveau de CORS filter mais ça je suppose que tu es au courant)

--- Conserto-back/src/main/java/com/example/demo/ConsertoBackApplication.java
+++ Conserto-back/src/main/java/com/example/demo/ConsertoBackApplication.java
@@ -2,11 +2,10 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration;
 
-@SpringBootApplication
-
+@SpringBootApplication(exclude = {WebMvcAutoConfiguration.class})
 public class ConsertoBackApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ConsertoBackApplication.class, args);
 	}
